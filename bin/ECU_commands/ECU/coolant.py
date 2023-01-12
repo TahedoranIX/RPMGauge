e65b5c5811883334a72d3805c8988db0cd239bad
Aleix <tahedoranix@gmail.com>
2023-01-12 22:06:29 +0100

    Funcional

--- bin/ECU_commands/ECU/coolant.py
+++ bin/ECU_commands/ECU/coolant.py
@@ -3,12 +3,12 @@ from Observers.observable import Observable
 
 
 class Coolant(ECU):
-    def __init__(self) -> None:
-        super().__init__()
-        self.coolOBD = 0
+	def __init__(self) -> None:
+		super().__init__()
+		self.coolOBD = 0
 
-    def update(self, commands: Observable):
-        self.coolOBD = commands.getCommands()["coolant"]
+	def update(self, commands: Observable):
+		self.coolOBD = commands.getCommands()["coolant"]
 
-    def print(self):
-        return 'Temp: ' + str(self.coolOBD) + ' C'
+	def print(self):
+		return 'Temp: ' + str(self.coolOBD) + ' C'
--- bin/ECU_commands/ecu.py
+++ bin/ECU_commands/ecu.py
@@ -1,19 +1,18 @@
 from abc import ABC, abstractmethod
 
-
 from Observers.observable import Observable
 from Observers.observer import Observer
 from obdsingle import OBDSingle
 
 
-
 class ECU(Observer, ABC):
-    def __init__(self) -> None:
-        OBDSingle.attach(self)
-    @abstractmethod
-    def update(self, commands: Observable):
-        pass
+	def __init__(self) -> None:
+		OBDSingle.attach(self)
 
-    @abstractmethod
-    def print(self) -> None:
-        pass
+	@abstractmethod
+	def update(self, commands: Observable):
+		pass
+
+	@abstractmethod
+	def print(self) -> None:
+		pass
