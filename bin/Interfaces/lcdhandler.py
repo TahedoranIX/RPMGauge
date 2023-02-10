abb592739b1c0243f5578dfeb7d0692a34ddc646
TahedoranIX <tahedoranix@gmail.com>
2023-02-10 01:45:54 +0100

    ecu passed to menu by list

--- bin/Interfaces/lcdhandler.py
+++ bin/Interfaces/lcdhandler.py
@@ -8,16 +8,16 @@ from lib.LCDLibrary.lcd import LCD
 
 class LCDHandler(PrintHub, object):
     lcd = None
+
     @classmethod
     def __init__(cls, d4, d5, d6, d7, en, rs):
         cls.lcd = LCD(d4, d5, d6, d7, en, rs)
 
     @classmethod
-    def print(cls, text):
+    def print(cls, text: List):
         cls.lcd.clearDisplay()
-        if isinstance(text, List):
+        if len(text) > 1:
             cls.lcd.writeMessage(text[0] + '\n' + text[1])
-        else:
+        elif len(text) is 1:
             cls.lcd.writeMessage(text)
         time.sleep(WAIT_REFRESH_PRINTHUB)
-
