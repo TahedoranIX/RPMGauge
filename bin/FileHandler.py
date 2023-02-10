f2cfd923f285fdf67aa0660fcc84a873c605019f
TahedoranIX <tahedoranix@gmail.com>
2023-02-10 01:46:30 +0100

    changed to cls

--- bin/FileHandler.py
+++ bin/FileHandler.py
@@ -1,11 +1,10 @@
 from os import path
 
-
 class FileHandler(object):
     file = str(path.realpath(path.dirname(__file__)) + "/mpg.dat")
     @classmethod
     def saveData(cls, mpg, muestras, tank=0):
-        f = open(FileHandler.file, 'w')
+        f = open(cls.file, 'w')
         f.write(str(mpg))
         f.write('\n')
         f.write(str(muestras))
@@ -16,16 +15,16 @@ class FileHandler(object):
     @classmethod
     def loadData(cls):
         try:
-            if path.exists(FileHandler.file):
-                f = open(FileHandler.file, 'r')
+            if path.exists(cls.file):
+                f = open(cls.file, 'r')
                 mpg = float(f.readline())
                 muestras = float(f.readline())
                 tank = float(f.readline())
             else:
-                f = open(FileHandler.file, 'x')
-                f.write('0\n0\n0')
+                f = open(cls.file, 'x')
+                f.write('0\n0\n0.000000000001')
                 mpg = 0
-                muestras = 0
+                muestras = 0.000000000001
                 tank = 0
             f.close()
             return mpg, muestras
@@ -33,6 +32,6 @@ class FileHandler(object):
         except Exception as e:
             print("cant create file")
             mpg = 0
-            muestras = 0
+            muestras = 0.000000000001
             tank = 0
             return mpg, muestras
